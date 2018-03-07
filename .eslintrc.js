2e98fa87de42b17d4f3e5e71f56b8fe2e130254e
valexiev <ghm@valexiev.com>
2018-03-07 17:13:50 +0200

    Add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,28 +2,36 @@
 
 module.exports = {
   root: true,
+  parser: 'babel-eslint',
   parserOptions: {
-    parser: 'babel-eslint'
+    sourceType: 'module'
   },
   env: {
     browser: true,
   },
-  extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
-    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard'
-  ],
+  // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+  extends: 'standard',
   // required to lint *.vue files
   plugins: [
-    'vue'
+    'html'
   ],
   // add your custom rules here
-  rules: {
+  'rules': {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
     // allow async-await
-    'generator-star-spacing': 'off',
+    'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // http://eslint.org/docs/rules/space-before-function-paren
+    'space-before-function-paren': ['error', 'never'],
+    // http://eslint.org/docs/rules/curly
+    'curly': ['error', 'all'],
+    // http://eslint.org/docs/rules/comma-dangle
+    'comma-dangle': ['error', 'always-multiline'],
+    // https://eslint.org/docs/rules/no-multi-spaces
+    'no-multi-spaces': ['error', { 'ignoreEOLComments': true }],
+    // https://eslint.org/docs/rules/no-multiple-empty-lines
+    'no-multiple-empty-lines': ['error', { 'max': 2, 'maxBOF': 1, 'maxEOF': 1 }]
   }
 }
