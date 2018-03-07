fc6447402f7597ea8f5cde5dafa8810372e0f055
valexiev <ghm@valexiev.com>
2018-03-07 17:36:14 +0200

    Move page components to their own directory

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+// PAGES
+import HomePage from '@/pages/HomePage'
 
 Vue.use(Router)
 
@@ -8,8 +10,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+      name: 'HomePage',
+      component: HomePage,
+    },
+  ],
 })
