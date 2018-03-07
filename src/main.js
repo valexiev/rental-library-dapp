ae553f092c77e7e832b62fced1af4d5de052476e
valexiev <ghm@valexiev.com>
2018-03-07 17:34:20 +0200

    Setup styles structure

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
+
+// Import always before all components
+import '@/styles/main-begin.scss'
+
+// NODE MODULES
+import BootstrapVue from 'bootstrap-vue'
+
+// APP PACKAGES
 import App from './App'
 import router from './router'
 
-Vue.config.productionTip = false
+// Import always after all components
+import '@/styles/main-end.scss'
+
+
+Vue.config.productionTip = !process.env.APP_DEBUG
+
+// USE PLUGINS
+Vue.use(BootstrapVue)
+
 
 /* eslint-disable no-new */
 new Vue({
