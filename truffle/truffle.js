90914340e5a48fffc3f3cb8a26943a4367c355d8
valexiev <ghm@valexiev.com>
2018-03-10 12:32:20 +0200

    Config truffle

--- truffle/truffle.js
+++ truffle/truffle.js
@@ -1,4 +1,10 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  // Docs: http://truffleframework.com/docs/advanced/configuration
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    }
+  }
 };
