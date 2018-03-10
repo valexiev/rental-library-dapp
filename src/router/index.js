f8448e677a5ba1b39066c779a12e010898a47118
valexiev <ghm@valexiev.com>
2018-03-10 13:38:50 +0200

    Config router

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,10 @@ import Router from 'vue-router'
 
 // PAGES
 import HomePage from '@/pages/HomePage'
+import AddBookPage from '@/pages/AddBookPage'
+import ArbitratorsPage from '@/pages/ArbitratorsPage'
+import AddArbitratorPage from '@/pages/AddArbitratorPage'
+import BooksPage from '@/pages/BooksPage'
 
 Vue.use(Router)
 
@@ -13,5 +17,30 @@ export default new Router({
       name: 'HomePage',
       component: HomePage,
     },
+    {
+      path: '/books',
+      name: 'BooksPage',
+      component: BooksPage,
+    },
+    {
+      path: '/add-book',
+      name: 'AddBookPage',
+      component: AddBookPage,
+    },
+    {
+      path: '/arbitrators',
+      name: 'ArbitratorsPage',
+      component: ArbitratorsPage,
+    },
+    {
+      path: '/add-arbitrator',
+      name: 'AddArbitratorPage',
+      component: AddArbitratorPage,
+    },
+    {
+      path: '*',
+      redirect: '/',
+    },
   ],
+  mode: 'history',
 })
